e5d5fbabecca48538ec4dafd910ab6d90efb6886
choewy <choewy32@gmail.com>
2022-04-20 23:11:30 +0900

    [수정] 폴더명 및 파일명 변경

---
+++ test/__intergration__/users.signup.spec.ts
@@ -0,0 +1,19 @@
+import request from 'supertest';
+import app from '../../app/app';
+import { UserSignupDto } from '../../app/src/modules/users/users.dto';
+import { UserSignupTestCases } from '../__case__/__intergration__/users.signup.case';
+
+const url = '/api/users/signup';
+const testCases = UserSignupTestCases();
+const onTest = (statusCode: number, body: UserSignupDto) =>
+  async function () {
+    const res = await request(app).post(url).send(body);
+    expect(res.statusCode).toEqual(statusCode);
+  };
+
+describe('사용자 회원가입 테스트', () => {
+  testCases.forEach((testCase, index) => {
+    const { statusCode, body } = testCase;
+    test(`#${index + 1}`, onTest(statusCode, body));
+  });
+});
