eaff76ed3e6e029c6728f91dbc1caddb2985156f
choewy <choewy32@gmail.com>
2022-04-20 23:14:47 +0900

    [수정] 기존 모델 폴더 구조 변경

---
+++ app/src/modules/app/app.models.ts
@@ -0,0 +1,35 @@
+import { Sequelize, Op } from 'sequelize';
+import SequelizeConfig from '../../configs/sequelize.config';
+import PostModel from '../posts/posts.model';
+import UserModel from '../users/users.model';
+
+const env: string = process.env.NODE_ENV || 'development';
+const config = SequelizeConfig(env);
+
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  host: config.host,
+  port: config.port,
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  define: {
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_general_ci',
+    underscored: true,
+    freezeTableName: true,
+  },
+});
+
+const db = {
+  Sequelize,
+  sequelize,
+  Op,
+  User: UserModel(sequelize),
+  Post: PostModel(sequelize),
+};
+
+db.User.hasMany(db.Post, { foreignKey: 'user_id', as: 'post' });
+db.Post.belongsTo(db.User, { foreignKey: 'user_id', as: 'user' });
+
+export default db;
