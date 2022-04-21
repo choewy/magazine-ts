4295f000ee42f44b89d23bf9745def09d0ec283b
choewy <choewy32@gmail.com>
2022-04-21 17:10:18 +0900

    [수정] UserController 줄바꿈

--- app/src/modules/users/users.controller.ts
+++ app/src/modules/users/users.controller.ts
@@ -4,6 +4,7 @@ import { UserService } from './users.service';
 
 export const UserController = (app: Express) => {
   const router = Router();
+
   router.post(
     '/signup',
     UserPipe.Email,
@@ -12,9 +13,7 @@ export const UserController = (app: Express) => {
     UserPipe.NicknameInPwd,
     UserService.Signup
   );
-
   router.post('/signin', UserPipe.Email, UserPipe.Password, UserService.Signin);
-
   router.get('/auth', UserPipe.isLogin, UserService.Auth);
 
   return app.use('/api/users', router);
