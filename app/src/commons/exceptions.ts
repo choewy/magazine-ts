3117af4c4506f4ff9782b26e5f69b09e6f1c6131
choewy <choewy32@gmail.com>
2022-04-21 13:32:01 +0900

    [수정] SystemError 예외 처리

--- app/src/commons/exceptions.ts
+++ app/src/commons/exceptions.ts
@@ -1,17 +1,44 @@
 import { ValidationError } from 'joi';
-import { HTTPError } from './errors';
+import { HTTPError, SystemError } from './errors';
 
-export class Exception extends HTTPError {
+export class Exception extends Error {
+  code: number;
+  body: {};
   constructor(error: unknown) {
-    let code, message;
-
     if (error instanceof ValidationError) {
-      (code = 400), (message = error.details[0].message);
+      const message = error.details[0].message;
+      super(message);
+      this.code = 400;
+      this.body = {
+        ok: false,
+        message,
+      };
     } else if (error instanceof HTTPError) {
-      (code = error.code), (message = error.message);
+      const message = error.message;
+      super(message);
+      this.code = error.code;
+      this.body = {
+        ok: false,
+        message,
+      };
+    } else if (error instanceof SystemError) {
+      const message = error.message;
+      super(message);
+      this.code = error.code;
+      this.body = {
+        ok: false,
+        message,
+        error,
+      };
     } else {
-      (code = 500), (message = '서버 내부적인 오류입니다.');
+      const message = '서버 내부적인 오류입니다.';
+      super(message);
+      this.code = 500;
+      this.body = {
+        ok: false,
+        message,
+        error,
+      };
     }
-    super(code, message);
   }
 }
