8fc51529281963a63420d34fe4ca322d99d4c6d7
choewy <choewy32@gmail.com>
2022-04-22 16:59:12 +0900

    [추가] 이미지 파일 전송

--- app/src/modules/app/app.public.ts
+++ app/src/modules/app/app.public.ts
@@ -1,9 +1,14 @@
 import { Request, Response, Express } from 'express';
 import path from 'path';
+import { uploadPath } from '../../configs/multer.config';
 
 export const publicPath = path.join(__dirname, '../../../public');
 export const AppController = (app: Express) => {
   app.get('/', (_: Request, res: Response) => {
     return res.sendFile(publicPath + '/index.html');
+  });
+  app.get('/image/:image_url', (req: Request, res: Response) => {
+    const { image_url } = req.params;
+    return res.sendFile(uploadPath + `/${image_url}`);
   });
 };
