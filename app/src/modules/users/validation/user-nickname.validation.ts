5a9488de6269649e98f8be9b40b4ae144f7148e8
choewy <choewy32@gmail.com>
2022-04-23 17:37:58 +0900

    [수정] 오류 문구 수정

--- app/src/modules/users/validation/user-nickname.validation.ts
+++ app/src/modules/users/validation/user-nickname.validation.ts
@@ -1,9 +1,9 @@
 import Joi from 'joi';
 
 export const UserNickname = () =>
-  Joi.string().min(3).max(10).required().messages({
-    'any.required': '닉네임을 입력하세요',
+  Joi.string().min(3).max(20).required().messages({
+    'any.required': '닉네임을 입력하세요.',
     'string.empty': '닉네임을 입력하세요.',
-    'string.min': '닉네임은 3~10자 이내로 입력하세요.',
-    'string.max': '닉네임은 3~10자 이내로 입력하세요.',
+    'string.min': '닉네임은 3~20자로 입력하세요.',
+    'string.max': '닉네임은 3~20자로 입력하세요.',
   });
